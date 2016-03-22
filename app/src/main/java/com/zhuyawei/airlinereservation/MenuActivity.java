52a82ca4a2813a8f482a256327a756e1979ea277
Yawei-Zhu <1024854177@qq.com>
2016-03-22 21:44:59 +0800

    First commit

---
+++ app/src/main/java/com/zhuyawei/airlinereservation/MenuActivity.java
@@ -0,0 +1,44 @@
+package com.zhuyawei.airlinereservation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void doClick(View view){
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.ibtn_search_branch:
+                i = new Intent(MenuActivity.this,SearchBranchActivity.class);
+                break;
+            case R.id.ibtn_search_flight_plan:
+                i = new Intent(MenuActivity.this,SearchFlightPlanActivity.class);
+                break;
+        }
+        startActivity(i);
+    }
+
+}
